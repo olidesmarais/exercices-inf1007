ac8d3dfa67ee0ef59c525d27f64bd20dcaa5a13f
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-14 20:48:35 -0400

    Exercices chapitre 2

--- ch02_1/exercice.py
+++ ch02_1/exercice.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+ecart_majuscule_minuscule = ord('a') - ord('A')
+
 def majuscule(mot):
     # TODO completer la fonction ici
-    return mot
+    resultat = ''
+    for lettre in mot:
+        # Si la lettre est une lettre minuscule
+        if ord('a') <= ord(lettre) <= ord('z'):
+            resultat += chr(ord(lettre) - ecart_majuscule_minuscule)
+        else:
+            resultat += lettre
+    return resultat
 
 
 if __name__ == '__main__':
