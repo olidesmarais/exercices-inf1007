3410fd0314c3e2c1c2d432e1bbd6c56a51c4a335
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-16 13:26:58 -0400

    ch4_02

--- ch04_2/exercice.py
+++ ch04_2/exercice.py
@@ -5,19 +5,46 @@ import random
 
 
 def get_first_part_of_name(name):
-	return ""
+	index = name.find('-')
+	return name[:index].title()
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	chance = random.randint(0, 2)
+	animal = animals[chance]
+	chance = random.randint(0, 2)
+	adjectif = adjectives[chance]
+	chance = random.randint(0, 2)
+	fruit = fruits[chance]
+	return f'Dans un univers parallèles, un {animal} est {adjectif} et chasse les {fruit}'
 
 def format_date(year, month, day, hours, minutes, seconds):
-	return ""
+	return f'{day:02}/{month:02}/{year:04} {hours:02}:{minutes:02}:{seconds:02.3f}'
 
 def encrypt(text, shift):
-	return ""
+	text = text.upper()
+	resultat = ""
+	for lettre in text:
+		if lettre.isalpha():
+			index = ord(lettre) - ord('A')
+			encrypted_index = (index + shift) % 26
+			resultat += chr(ord('A') + encrypted_index)
+		else:
+			resultat += lettre
+	return resultat
 
 def decrypt(encrypted_text, shift):
-	return ""
+	return encrypt(encrypted_text, -shift)
+
+# def decrypt(encrypted_text, shift):
+# 	resultat = ''
+# 	for lettre in encrypted_text:
+# 		if lettre.isalpha():
+# 			index = ord(lettre) - ord('A')
+# 			decrypted_index = (index - shift) % 26
+# 			resultat += chr(ord('A') + decrypted_index)
+# 		else:
+# 			resultat += lettre
+# 	return resultat
 
 
 if __name__ == "__main__":
