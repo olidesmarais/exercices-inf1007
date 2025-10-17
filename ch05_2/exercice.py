96e373874f837afdc8b515b5df2d103a811c1a7b
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-16 23:29:55 -0400

    ch05_2

--- ch05_2/exercice.py
+++ ch05_2/exercice.py
@@ -9,20 +9,62 @@ def format_bill_total(data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	sous_total = 0
+	for item in data:
+		sous_total += item[1] * item[2]
+	taxe = sous_total * 0.15
+	total = sous_total + taxe
+
+	resultat = ''
+	resultat += f"{'SOUS TOTAL':14} {sous_total:7.2f} $\n"
+	resultat += f"{'TAXE':14} {taxe:7.2f} $\n"
+	resultat += f"{'TOTAL':14} {total:7.2f} $\n"
+
+	return resultat
 
 def format_bill_items(data):
+	resultat = ""
+
 	INDEX_NAME = 0
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	longueur_max = 0
+	for item in data:
+		if longueur_max < len(item[0]):
+			longueur_max = len(item[0])
+
+	for item in data:
+		resultat += f'{item[INDEX_NAME]:{longueur_max}}{item[1] * item[2]:10.2f} $\n'
+
+	return resultat
 
 def format_number(number, num_decimal_digits):
-	return ""
+	negatif = number < 0
+	positif = abs(number)
+
+	entier = int(positif)
+	entier_string = ''
+	while entier > 1000:
+		sous_entier = entier % 1000
+		entier_string = f' {sous_entier:03}{entier_string}'
+		entier //= 1000
+	entier_string = f'{entier}{entier_string}'
+
+	decimal = round(positif % 1, 2)
+	decimal *= 10**num_decimal_digits
+	decimal = int(decimal)
+
+	return f"{'-' if negatif else ''}{entier_string}.{decimal:0{num_decimal_digits}}"
 
 def get_triangle(num_rows):
-	return ""
+	largeur_triangle = 1 + 2 * (num_rows - 1)
+	resultat = ''
+	resultat += f"{'+'* (largeur_triangle + 2)}\n"
+	for idx in range(0, num_rows):
+		resultat += f"+{'A' * (idx * 2 + 1): ^{largeur_triangle}}+\n"
+	resultat += f"{'+'* (num_rows * 2 + 1)}\n"
+	return resultat
 
 
 if __name__ == "__main__":
