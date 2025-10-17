dd2937fdfc2b9ff2b76a30d468616d25ef09c5b9
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-17 17:04:17 -0400

    ch06_4

--- ch06_4/exercice.py
+++ ch06_4/exercice.py
@@ -7,19 +7,23 @@ import itertools
 
 
 def get_even_keys(dictionary):
-	return {}
+	return set(cle for cle in dictionary if cle % 2 == 0)
 
 def join_dictionaries(dictionaries):
-	return {}
+	return {
+		key: value
+			for dictionnaire in dictionaries
+				for key, value in dictionnaire.items()
+	}
 
 def dictionary_from_lists(keys, values):
-	return {}
+	return dict(zip(keys, values))
 
 def get_greatest_values(dictionnary, num_values):
-	return []
+	return sorted(dictionnary.values(), reverse=True)[:num_values]
 
 def get_sum_values_from_key(dictionnaries, key):
-	return 0
+	return sum(dictionnaire[key] for dictionnaire in dictionnaries if key in dictionnaire)
 
 
 if __name__ == "__main__":
