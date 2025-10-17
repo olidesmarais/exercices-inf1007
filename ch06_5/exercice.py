9f3877b225248b1d3dc57187b3bccb1a9c2ab6b7
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-17 18:50:31 -0400

    ch06_5

--- ch06_5/exercice.py
+++ ch06_5/exercice.py
@@ -1,18 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 def check_brackets(text, brackets):
-	return False
+	print(text)
+	structure = []
+	for caractere in text:
+		if caractere in brackets:
+			if brackets.index(caractere) % 2 == 0:
+				structure.append(caractere)
+			else:
+				ouverture = structure.pop()
+				if brackets.index(caractere) != brackets.index(ouverture) + 1:
+					return False
+	if len(structure) > 0:
+		return False
+	return True
 
 def remove_comments(full_text, comment_start, comment_end):
-	return ""
+	index_start = full_text.find(comment_start)
+	index_end = full_text.find(comment_end)
+	if index_start < 0 or index_end < 0 or index_end < index_start:
+		return None
+	return full_text[:index_start] + full_text[index_end + len(comment_end):]
 
 def get_tag_prefix(text, opening_tags, closing_tags):
+	for otag, ctag in zip(opening_tags, closing_tags):
+		if text.startswith(otag):
+			return (otag, None)
+		if text.startswith(ctag):
+			return (None, ctag)
 	return (None, None)
 
 def check_tags(full_text, tag_names, comment_tags):
-	return False
+	text_sans_commentaire = full_text
+	while text_sans_commentaire is not None and (comment_tags[0] in text_sans_commentaire or comment_tags[1] in text_sans_commentaire):
+		text_sans_commentaire = remove_comments(text_sans_commentaire, comment_tags[0], comment_tags[1])
+	if text_sans_commentaire == None:
+		return False
+
+	# Balises ouvrantes vers fermantes
+	balises_ouvrantes = {f'<{tag_name}>':f'</{tag_name}>' for tag_name in tag_names}
+	# Balises fermantes vers ouvrantes
+	balises_fermantes = dict((v, k) for k, v in balises_ouvrantes.items())
+
+	tag_stack = []
+	texte = text_sans_commentaire
+	while len(texte) > 0:
+		ouvrant, fermant = get_tag_prefix(texte, balises_ouvrantes.keys(), balises_fermantes.keys())
+		if ouvrant is not None:
+			tag_stack.append(ouvrant)
+			texte = texte[len(ouvrant):]
+		elif fermant is not None:
+			if len(tag_stack) == 0 or tag_stack[-1] != balises_fermantes[fermant]:
+				return False
+			tag_stack.pop()
+			texte = texte[len(fermant):]
+		else:
+			# On avance jusqu'à la prochaine balise.
+			texte = texte[1:]
+
+	return len(tag_stack) == 0
 
 
 if __name__ == "__main__":
@@ -85,8 +132,8 @@ if __name__ == "__main__":
 	)
 	tags = ("html", "head", "title", "body", "h1")
 	comment_tags = ("<!--", "-->")
-	print(check_tags(spam, tags, comment_tags))
-	print(check_tags(eggs, tags, comment_tags))
-	print(check_tags(parrot, tags, comment_tags))
+	print('premier', check_tags(spam, tags, comment_tags))
+	print('deuxieme', check_tags(eggs, tags, comment_tags))
+	print('troisieme', check_tags(parrot, tags, comment_tags))
 	print()
 
