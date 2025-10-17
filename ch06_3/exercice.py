e859c543bfeaabacfd8221866e2688f8ffda1450
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-10-17 16:16:09 -0400

    ch06_3

--- ch06_3/exercice.py
+++ ch06_3/exercice.py
@@ -7,16 +7,38 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [0]
+	resultat = []
+	for liste in numbers:
+		resultat.append(max(liste))
+	return resultat
 
 def join_integers(numbers):
-	return 0
+	return int(''.join([str(number) for number in numbers]))
 
 def generate_prime_numbers(limit):
-	return [0]
+	# FONCTION Eratosthène(limite)
+    # premiers = []
+	premiers = []
+    # nombres = liste des entiers de 2 à limite
+	nombres = [nombre for nombre in range( 2, limit + 1)]
+    # TANT QUE nombres est non vide FAIRE
+	while len(nombres) > 0:
+    #     Ajouter à premiers le premier entier de nombres
+		premier = nombres.pop(0)
+		premiers.append(premier)
+    #     nombres = liste des entiers de nombres non multiples du premier
+		nombres = [nombre for nombre in nombres if nombre % premier != 0]
+    # RÉSULTAT premiers
+	return premiers
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+	resultat = []
+	for idx in range( 1, num_combinations + 1):
+		if excluded_multiples and idx % excluded_multiples == 0:
+			continue
+		for lettre in strings:
+			resultat.append(f'{lettre}{idx}')
+	return resultat
 
 
 if __name__ == "__main__":
